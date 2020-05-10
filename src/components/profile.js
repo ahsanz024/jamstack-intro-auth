633010bfe206b7693560c36046383038a0c8d8ea
Ahsan <ahsan024@gmail.com>
2020-05-10 17:42:52 +0200

    fix: profile page

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React from "react";
+import { Link } from "gatsby";
+import { useIdentityContext } from "react-netlify-identity";
 
-const Profile = () => {
+const Profile = ({ showModal }) => {
+  const identity = useIdentityContext();
+  const isLoggedIn = identity && identity.isLoggedIn;
+
+  const name =
+    identity &&
+    identity.user &&
+    identity.user.user_metadata &&
+    identity.user.user_metadata.full_name;
+
   return (
-    <div className="dashboard-header">
-      <nav>
-        <Link to="/dashboard/secret" activeClassName="active">Secret Stuff</Link>
-        <Link to="/dashboard/base" activeClassName="active">See your base</Link>
-      </nav>
-      <div>TODO: show login status</div>
-    </div>
-  )
+    isLoggedIn && (
+      <div className="dashboard-header">
+        <nav>
+          <Link to="/dashboard/secret" activeClassName="active">
+            Secret Stuff
+          </Link>
+          <Link to="/dashboard/base" activeClassName="active">
+            See your base
+          </Link>
+        </nav>
+
+        <span>
+          Logged In as {name}. <button onClick={showModal}>Logout</button>
+        </span>
+      </div>
+    )
+  );
 };
 
 export default Profile;
