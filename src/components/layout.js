dac0a040032c3e6788901728b5b89b61e8db9b59
Ahsan <ahsan024@gmail.com>
2020-05-10 16:40:09 +0200

    add: basi routing

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'gatsby';
+import { Link } from 'gatsby';
 import { IdentityContextProvider } from 'react-netlify-identity-widget';
 
 import './layout.css';
